027fde2e0aa6ca650d9b01756974eeb12ac54f3b
Jerwin <jerwinfernandez04@gmail.com>
2020-03-11 00:43:22 +0800

    Creating sample config and db files

--- api/config/global_sample.php
+++ api/config/global_sample.php
@@ -5,10 +5,8 @@
   define('INIT', 'http://rest-api-jwt/api/bootstrap.php');
 
   // variables used for jwt
-  $key = "example_key";
-  $iss = "http://example.org";
-  $aud = "http://example.com";
-  $iat = 1356999524;
-  $nbf = 1357000000;
-
-  
+  $key = "";
+  $iss = "";
+  $aud = "";
+  $iat = 0;
+  $nbf = 0;
